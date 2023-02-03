bf8c23e7597b46959a2192e51719ccc2729d379a
EzeMordred <84354807+EzeMordred@users.noreply.github.com>
2023-02-03 15:11:35 -0500

    Add files via upload
    Servicios de los Costos del Curso

---
+++ detalleCostosCursos/editDetailCourseCosts.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Headers: Origin, X-Requested-with, Content-type, Authorization');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+include "connection.php";
+
+$idCurso = mysqli_real_escape_string($conn, $_POST['ID_CURSO']); //Obligatorio
+$costoGeneral = mysqli_real_escape_string($conn, $_POST['COSTO_GENERAL']);
+$costoEstudiante = mysqli_real_escape_string($conn, $_POST['COSTO_ESTUDIANTE']); 
+$costoEgresado = mysqli_real_escape_string($conn, $_POST['COSTO_EGRESADO']); 
+$costoGraduado = mysqli_real_escape_string($conn, $_POST['COSTO_GRADUADO']);
+$costoDocente = mysqli_real_escape_string($conn, $_POST['COSTO_DOCENTE']);
+$costoPersonalAdministrativo = mysqli_real_escape_string($conn, $_POST['COSTO_PERSONAL_ADMINISTRATIVO']);
+
+$sentencia = $conn->prepare("UPDATE detalle_costos_cursos SET id_curso = ?, costo_general = ?, costo_estudiante = ?, costo_egresado = ?, costo_graduado = ?, costo_docente = ?, costo_personal_administrativo = ? WHERE id_curso = ?");
+$sentencia->bind_param('iiiiiii', $idCurso, $costoGeneral, $costoEstudiante, $costoEgresado, $costoGraduado, $costoDocente, $costoPersonalAdministrativo);
+
+$resultado = $sentencia->execute();
+
+if ($resultado) {
+    echo json_encode("Se edito correctamente");
+}
+
+$sentencia->close();
+$conn->close();
+?>
